c7c81c1c49cdf12d2ee51f7e7e45da9698ce02b5
yehor-kuzmych <yehor.kuzmych@gmail.com>
2025-07-10 13:57:19 +0200

    Yk090725 (#9)
    * implement preprocessor func and add unit tests for it
    
    * fix lint
    
    * fix lint
    
    * fix lint
    
    ---------
    
    Co-authored-by: Yehor Kuzmych <yehor_kuzmych@MacBook-Air-Yehor.local>

---
+++ app/tools/preprocessor.py
@@ -0,0 +1,54 @@
+import re
+from dataclasses import dataclass
+from typing import List
+
+import spacy
+
+nlp = spacy.load('en_core_web_sm')
+
+
+@dataclass
+class ParsedSentence:
+    paragraphIndex: int
+    sentenceIndex: int
+    text: str
+    charStart: int
+    charEnd: int
+
+
+def replace_html_tags_with_spaces(text: str) -> str:
+    def replacer(match: re.Match) -> str:
+        return ' ' * len(match.group(0))
+
+    return re.sub(r'<[^>]+>', replacer, text)
+
+
+def preprocessing(text: str) -> List[ParsedSentence]:
+    cleaned = text.replace('\r\n', '\n').replace('\r', '\n')
+    cleaned = re.sub(r'\n\s*\n+', '\n\n', cleaned)
+    cleaned = cleaned.strip()
+    cleaned = replace_html_tags_with_spaces(cleaned)
+    paragraphs = cleaned.split('\n\n')
+    offset = 0
+    result = []
+
+    for p_idx, paragraph in enumerate(paragraphs):
+        doc = nlp(paragraph)
+        for s_idx, sent in enumerate(doc.sents):
+            abs_start = text.find(sent.text.split()[0], offset)
+            item = sent.text
+            item = item.strip()
+            abs_end = abs_start + len(item)
+            item = re.sub(r'[ \t]+', ' ', item)
+            result.append(
+                ParsedSentence(
+                    paragraphIndex=p_idx,
+                    sentenceIndex=s_idx,
+                    text=item,
+                    charStart=abs_start,
+                    charEnd=abs_end - 1
+                )
+            )
+            offset = abs_end
+
+    return result
