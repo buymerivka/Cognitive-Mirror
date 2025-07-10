0f5679a169797c8d6dadf22da7dd9796458ef74e
ArseniiGalaida <galaida.arseniy@gmail.com>
2025-07-10 12:05:36 +0000

    route name fixed

--- app/request/router.py
+++ app/request/router.py
@@ -6,6 +6,6 @@ from app.request.schemas import RequestSchema
 router = APIRouter(prefix='/analyze', tags=['analyser'])
 
 
-@router.post('/analyze', response_model=[])
+@router.post('/', response_model=[])
 def analyze(request_data: RequestSchema):
     return JSONResponse(status_code=200, content={'details': 'Data received successfully.'})
