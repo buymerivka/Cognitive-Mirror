19f8c3f6a4461332cb0eb32b8a3748a7d4fdbac2
ArseniiGalaida <galaida.arseniy@gmail.com>
2025-07-10 21:21:40 +0000

    update api to return result of preprocessor

--- app/request/schemas.py
+++ app/request/schemas.py
@@ -6,3 +6,7 @@ from pydantic import BaseModel
 @dataclass
 class RequestSchema(BaseModel):
     input_data: str
+
+
+class ResponseSchema(BaseModel):
+    parsed_data: list[dict]
--- tests/test_api.py
+++ tests/test_api.py
@@ -16,10 +16,66 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(response.json(), {'status': 'ok'})
 
     def test_analyze_success(self):
-        payload = {'input_data': 'Some input text'}
+        payload = {
+            'input_data': 'People always act like everything is either good or bad. That’s just not true.\n'
+                          'We must be prepared, or we will face terrible consequences. Are you ready to lose '
+                          'everything?\n\nSome experts suggest waiting. Others believe action is needed now.\n'
+                          'Either you’re with us, or against us.'}
         response = self.client.post('/analyze/', json=payload)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {'details': 'Data received successfully.'})
+        self.assertEqual(response.json(), {
+            'parsed_data': [
+                {
+                    'paragraphIndex': 0,
+                    'sentenceIndex': 0,
+                    'text': 'People always act like everything is either good or bad.',
+                    'charStart': 0,
+                    'charEnd': 55
+                },
+                {
+                    'paragraphIndex': 0,
+                    'sentenceIndex': 1,
+                    'text': 'That’s just not true.',
+                    'charStart': 57,
+                    'charEnd': 77
+                },
+                {
+                    'paragraphIndex': 0,
+                    'sentenceIndex': 2,
+                    'text': 'We must be prepared, or we will face terrible consequences.',
+                    'charStart': 79,
+                    'charEnd': 137
+                },
+                {
+                    'paragraphIndex': 0,
+                    'sentenceIndex': 3,
+                    'text': 'Are you ready to lose everything?',
+                    'charStart': 139,
+                    'charEnd': 171
+                },
+                {
+                    'paragraphIndex': 1,
+                    'sentenceIndex': 0,
+                    'text': 'Some experts suggest waiting.',
+                    'charStart': 174,
+                    'charEnd': 202
+                },
+                {
+                    'paragraphIndex': 1,
+                    'sentenceIndex': 1,
+                    'text': 'Others believe action is needed now.',
+                    'charStart': 204,
+                    'charEnd': 239
+                },
+                {
+                    'paragraphIndex': 1,
+                    'sentenceIndex': 2,
+                    'text': 'Either you’re with us, or against us.',
+                    'charStart': 241,
+                    'charEnd': 277
+                }
+            ]
+        })
 
     def test_analyze_validation_error(self):
         response = self.client.post('/analyze/', json={})
