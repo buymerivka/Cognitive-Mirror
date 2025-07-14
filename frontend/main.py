237f140f647125cfe92c34a458a774006ac7f0d4
ArseniiGalaida <galaida.arseniy@gmail.com>
2025-07-14 10:55:22 +0000

    implement frontend

---
+++ frontend/main.py
@@ -0,0 +1,31 @@
+import os
+
+from dotenv import load_dotenv
+from fastapi import Request
+from nicegui import ui
+
+from views import (
+    render_analyze_request,
+    render_error
+)
+
+load_dotenv()
+API_BASE_URL = os.getenv('API_BASE_URL')
+
+
+@ui.page('/')
+def base_page():
+    ui.navigate.to('/analyze')
+
+
+@ui.page('/analyze')
+def analyze_request(request: Request):
+    render_analyze_request()
+
+
+@ui.page('/error')
+def error(request: Request):
+    render_error()
+
+
+ui.run(title='Cognitive-Mirror', port=8080)
