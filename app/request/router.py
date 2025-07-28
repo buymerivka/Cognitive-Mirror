a2f18e7ea13c417ff8cd1062ffbab04bec80aca7
Arsenii Galaida <arsenii.galaida@gmail.com>
2025-07-29 00:57:50 +0300

    updated returned data for all 3 endpoints

--- app/request/router.py
+++ app/request/router.py
@@ -12,7 +12,14 @@ router = APIRouter(tags=['analyser'])
 async def analyze_propaganda(request_data: RequestSchema, top_n: int = 1):
     clf = bias_classifier.BiasClassifier()
     clf.load()
-    analyzed_text = clf.predict([data.text for data in preprocessing(request_data.input_data)], top_n=top_n)
+    initial_data = preprocessing(request_data.input_data)
+    analyzed_text = clf.predict([data.text for data in initial_data], top_n=top_n)
+
+    for idx, data in enumerate(analyzed_text):
+        data['paragraphIndex'] = initial_data[idx].paragraphIndex
+        data['sentenceIndex'] = initial_data[idx].sentenceIndex
+        data['charStart'] = initial_data[idx].charStart
+        data['charEnd'] = initial_data[idx].charEnd
 
     return ResponseSchema.model_validate({'analyzed_data': analyzed_text})
 
@@ -27,9 +34,16 @@ async def analyze_emotions(request_data: RequestSchema, top_n: int = 1):
 async def analyze(request_data: RequestSchema, top_n: int = 1):
     clf = bias_classifier.BiasClassifier()
     clf.load()
-    analyzed_propaganda = clf.predict([data.text for data in preprocessing(request_data.input_data)], top_n=top_n)
-    analyzed_emotion = text_classify_by_paragraph(request_data.input_data, top_n)
+    initial_data = preprocessing(request_data.input_data)
+    analyzed_propaganda = clf.predict([data.text for data in initial_data], top_n=top_n)
 
+    for idx, data in enumerate(analyzed_propaganda):
+        data['paragraphIndex'] = initial_data[idx].paragraphIndex
+        data['sentenceIndex'] = initial_data[idx].sentenceIndex
+        data['charStart'] = initial_data[idx].charStart
+        data['charEnd'] = initial_data[idx].charEnd
+
+    analyzed_emotion = text_classify_by_paragraph(request_data.input_data, top_n)
     return TotalResponseSchema.model_validate({
         'propaganda_analyzed': analyzed_propaganda,
         'emotions_analyzed': analyzed_emotion,
--- app/tools/emotion_classifier.py
+++ app/tools/emotion_classifier.py
@@ -10,8 +10,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ensure_model()
 
+'''
+    paragraphIndex: int
+    sentenceIndex: int
+    text: str
+    charStart: int
+    charEnd: int
+'''
 
-def emotion_classify(text: str, n: int = 1):
+
+def emotion_classify(text: str, paragraph_index: int, sentence_index: int, char_start: int, char_end: int, n: int = 1):
     if n > 28:
         n = 28
     local_model_path = f'{BASE_DIR}/models/bert-goemotions'
@@ -35,33 +43,48 @@ def emotion_classify(text: str, n: int = 1):
         'predictions': [{
             'label': list(top_emotions.keys())[i],
             'score': list(top_emotions.values())[i],
-        } for i in range(len(top_emotions))]
+        } for i in range(len(top_emotions))],
+        'paragraphIndex': paragraph_index,
+        'sentenceIndex': sentence_index,
+        'charStart': char_start,
+        'charEnd': char_end,
     }
 
 
 def text_classify_by_sentence(text: str, n: int = 1):
-    sentences = [data.text for data in preprocessing(text)]
+    sentences_data = [data for data in preprocessing(text)]
     result = []
-    for sentence in sentences:
-        result.append(emotion_classify(sentence, n))
+    for sentence_data in sentences_data:
+        result.append(emotion_classify(sentence_data.text, sentence_data.paragraphIndex, sentence_data.sentenceIndex,
+                      sentence_data.charStart, sentence_data.charEnd, n))
 
     return result
 
 
 def text_classify_by_paragraph(text: str, n: int = 1):
     parsed_data = preprocessing(text)
-    print(parsed_data)
     paragraphs = []
     current_paragraph_idx = -1
+    print(parsed_data)
     for data in parsed_data:
         if data.paragraphIndex == current_paragraph_idx:
-            paragraphs[-1] += ' ' + data.text
+            paragraphs[-1]['text'] += ' ' + data.text
+            paragraphs[-1]['char_end'] = data.charEnd
         else:
-            paragraphs.append(data.text)
+            paragraphs.append({'text': data.text,
+                               'sentence_id': -1,
+                               'paragraph_id': data.paragraphIndex,
+                               'char_start': data.charStart,
+                               'char_end': data.charEnd})
             current_paragraph_idx += 1
     result = []
-    print(paragraphs)
-    for paragraph in paragraphs:
-        result.append(emotion_classify(paragraph, n))
 
+    for paragraph in paragraphs:
+        result.append(emotion_classify(paragraph['text'],
+                                       paragraph['paragraph_id'],
+                                       paragraph['sentence_id'],
+                                       paragraph['char_start'],
+                                       paragraph['char_end'],
+                                       n))
+    print(result)
     return result
