bbfb29ad0d6795e6cb7bd41d5b2835dcb1a1aea3
Yehor Kuzmych <yehor_kuzmych@MacBook-Air-Yehor.local>
2025-08-28 14:40:16 +0200

    implement propaganda model and improve manipulations model

--- app/tools/bias_classifier_bert.py
+++ app/tools/bias_classifier_bert.py
@@ -1,35 +1,104 @@
-# import os
-#
-# import joblib
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.pipeline import FeatureUnion
-# from sklearn.preprocessing import LabelEncoder
-#
-# from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
-#
-# import evaluate
-# import numpy as np
-# from transformers import DataCollatorWithPadding
-#
-# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-#
-#
-# class BiasClassifier:
-#     def __init__(self, model_name: str = "bert-base-uncased", max_len: int = 160,
-#                  test_size: float = 0.1,
-#                  seed: int = 42, ):
-#         self.model_name = model_name
-#         self.max_len = max_len
-#         self.test_size = test_size
-#         self.seed = seed
-#
-#         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name, use_fast=True)
-#         self.label_encoder = LabelEncoder()
-#
-#         self.model = None
-#         self.trainer = None
-#
-#     def train(self, texts, labels):
-#
-#
+import argparse
+import os
+
+import numpy as np
+import pandas as pd
+from datasets import Dataset, ClassLabel, load_dataset
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
+from sklearn.metrics import f1_score
+from huggingface_hub import HfFolder
+import json
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+def main():
+
+
+
+    dataset = load_dataset('json', data_files={'train': f'{BASE_DIR}/datasets/train_CoCoLoFa.json',
+                                               'dev': f'{BASE_DIR}/datasets/dev_CoCoLoFa.json',
+                                               'test': f'{BASE_DIR}/datasets/test_CoCoLoFa.json'})
+
+    if "label" in dataset["train"].features.keys():
+        dataset = dataset.rename_column("label", "labels")
+
+    if "label" in dataset["dev"].features.keys():
+        dataset = dataset.rename_column("label", "labels")
+
+    if "label" in dataset["test"].features.keys():
+        dataset = dataset.rename_column("label", "labels")
+
+    dataset = dataset.class_encode_column('labels')
+
+    model_id = 'vinai/bertweet-base'
+    tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=False)
+
+
+    def tokenize(batch):
+        return tokenizer(batch['text'], padding='max_length', truncation=True, max_length=128)
+
+
+    tokenized_dataset = dataset.map(tokenize, batched=True, remove_columns=['text'])
+
+    labels = tokenized_dataset["train"].features["labels"].names
+    num_labels = len(labels)
+    label2id, id2label = dict(), dict()
+    for i, label in enumerate(labels):
+        label2id[label] = str(i)
+        id2label[str(i)] = label
+
+    model = AutoModelForSequenceClassification.from_pretrained(
+        model_id, num_labels=num_labels, label2id=label2id, id2label=id2label,
+    )
+
+
+    def compute_metrics(eval_pred):
+        predictions, expected_labels = eval_pred
+        predictions = np.argmax(predictions, axis=1)
+        score = f1_score(
+                expected_labels, predictions, average="weighted"
+            )
+        return {"f1": float(score) if score == 1 else score}
+
+
+    training_args = TrainingArguments(
+        output_dir="../models/manipulations_bert_model",
+        per_device_train_batch_size=32,
+        per_device_eval_batch_size=32,
+        learning_rate=5e-5,
+        num_train_epochs=3,
+        weight_decay=0.01,
+        logging_dir="./logs",
+        logging_steps=50,
+        eval_strategy="epoch",
+        save_strategy="epoch",
+        save_total_limit=2,
+        load_best_model_at_end=True,
+        dataloader_num_workers=4,
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_dataset["train"],
+        eval_dataset=tokenized_dataset["dev"],
+        compute_metrics=compute_metrics
+    )
+
+    trainer.train()
+
+if __name__ == "__main__":
+    import multiprocessing
+    multiprocessing.freeze_support()
+    usecols = ['propaganda_category']
+    datasetPD = pd.read_csv(f'{BASE_DIR}/corpus/df_tweets_HiQualProp.csv', usecols=usecols)
+
+    data = datasetPD['propaganda_category']
+    dataNotNone = [sample for sample in data if sample != 'none']
+
+    print(len(dataNotNone))
+
+
+
+
--- app/tools/test_model.py
+++ app/tools/test_model.py
@@ -1,41 +1,61 @@
+import argparse
+import os
 import json
 
+import numpy as np
+import pandas as pd
+from datasets import Dataset, ClassLabel
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
+from sklearn.metrics import f1_score, confusion_matrix, ConfusionMatrixDisplay
+from huggingface_hub import HfFolder
 import matplotlib.pyplot as plt
-from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score, confusion_matrix
-from sklearn.metrics import f1_score
 
-from app.tools.bias_classifier import BiasClassifier
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+import torch
+
 
 if __name__ == '__main__':
-    bias = BiasClassifier()
-    bias.load()
 
-    with open('../datasets/dev_CoCoLoFa.json', 'r', encoding='utf-8') as f:
-        data = json.load(f)
+    usecols = ['strategy', 'text_normalized']
+    datasetPD = pd.read_csv(f'{BASE_DIR}/corpus/df_tweets_HiQualProp.csv', usecols=usecols)
 
-    texts = [item['text'] for item in data]
-    labels = [item['label'] for item in data]
 
-    predictions = bias.predict(texts)
-    predicted_labels = [item['predictions'][0]['label'] for item in predictions]
-    f1_macro = f1_score(labels, predicted_labels, average='macro')
-    accuracy = accuracy_score(labels, predicted_labels)
+    data = datasetPD['text_normalized']
+    labels = datasetPD['strategy']
+    data_test = data[:2000].tolist()
+    labels_test = labels[:2000].tolist()
 
-    print(f"{'True Label':<30} {'Predicted Label':<30} Match")
-    print('-' * 70)
-    for true_label, pred in zip(labels, predicted_labels):
-        match = '✅' if true_label == pred else '❌'
-        print(f'{true_label:<30} {pred:<30} {match}')
 
-    print(f'Accuracy: {accuracy:.2%}')
-    print(f1_macro)
-    print(bias.label_encoder.classes_)
 
-    # cm = confusion_matrix(labels, predicted_labels)
-    # disp = ConfusionMatrixDisplay(confusion_matrix=cm,
-    #                               display_labels=bias.label_encoder.classes_)
-    #
-    # disp.plot(xticks_rotation='vertical', cmap='Blues')
-    # plt.title('Confusion Matrix')
-    # plt.tight_layout()
-    # plt.show()
+    model_path = '../models/propaganda_bert_model/checkpoint-2220'
+    tokenizer_path = '../models/propaganda_bert_model/tokenizer'
+
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+    model = AutoModelForSequenceClassification.from_pretrained(model_path)
+
+    trainer = Trainer(model=model, tokenizer=tokenizer)
+
+    inputs = tokenizer(data_test , padding='max_length', truncation=True, max_length=128,  return_tensors="pt").to('mps')
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    predicted_classes_id = outputs.logits.argmax(dim=-1).cpu().numpy()
+    predicted_classes_str = [str(label) for label in predicted_classes_id]
+
+    predicted_labels = [model.config.id2label[pred_class] for pred_class in predicted_classes_id]
+
+    for pred_class, exp_class in zip(predicted_labels, labels_test):
+        if pred_class == exp_class:
+            print("Predicted label:", pred_class, 'TRUE')
+        else:
+            print("Predicted label:", pred_class, 'FALSE')
+    score = f1_score(labels_test, predicted_labels, average='weighted')
+    print(score)
+
+    true_class_id = [model.config.label2id[label] for label in labels_test]
+
+    cm = confusion_matrix(true_class_id, predicted_classes_str)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=list(model.config.id2label.values()))
+    disp.plot(xticks_rotation=45, cmap='Blues')
+    plt.show()
