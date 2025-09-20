ed3e4a118c3c01688c91f56a51d710e3a8755f30
Yehor Kuzmych <yehor_kuzmych@MacBook-Air-Yehor.local>
2025-09-20 16:52:22 +0200

    not final version

--- frontend/views.py
+++ frontend/views.py
@@ -59,7 +59,7 @@ def render_footer():
 def render_analyze_manipulations_request():
     render_header()
     with ui.column().classes('w-full justify-center items-center'):
-        ui.label('Create a request').classes('text-[17px] mb-[10px] text-[24px] font-bold justify-center')
+        # ui.label('Create a request').classes('text-[17px] mb-[10px] text-[24px] font-bold justify-center')
         card_container = ui.row().classes('card_container mt-[20px] w-full justify-center')
         with ui.row().classes('w-[1000px] max-w-[1000px] gap-0 justify-center self-center'):
             ui.add_head_html('''
@@ -73,9 +73,10 @@ def render_analyze_manipulations_request():
                     }
                 </style>
             ''')
-            with ui.column().classes('gap-0'):
-                ui.label('Provide a text').classes('text-[18px] mt-[20px] justify-center')
-                description = ui.textarea().classes('w-[600px]').props('id=create-request outlined dense autogrow')
+            start_column = ui.column().classes('gap-0')
+            with start_column:
+                ui.label('Provide a text for analysis:').classes('text-[18px] mt-[200px] justify-center font-bold')
+                description = ui.textarea().classes('w-[1000px]').props('id=create-request outlined dense autogrow')
 
             analyzed_data = None
 
@@ -88,8 +89,11 @@ def render_analyze_manipulations_request():
                 nonlocal first_run, analyzed_data, checkbox_states
                 for technique in checkbox_states:
                     checkbox_states[technique] = True
+                    if technique in checkbox_elements:
+                        checkbox_elements[technique].value = True
                 analyzed_data = None
                 first_run = True
+                description.value = ''
 
             def create_on_click():
                 nonlocal analyzed_data, checkbox_states, first_run, checkbox_elements
@@ -103,24 +107,24 @@ def render_analyze_manipulations_request():
                 else:
                     card_container.clear()
                     with card_container:
-                        with ui.column().classes(
-                                'mt-[20px] max-w-[300px] rounded-[12px] p-[20px] shadow-md'):
-                            ui.label('Filters:').classes('text-xl font-bold mb-4')
-                            ui.label('Show manipulation techniques:').classes('text-[16px] mt-[10px] mb-[5px]')
-
-                            with ui.column().classes(
-                                    'max-h-[300px] w-[200px] overflow-y-auto rounded-[12px] p-[20px] shadow-md'):
-                                for technique in checkbox_states.keys():
-                                    if technique != 'none':
-                                        checkbox_elements[technique] = ui.checkbox(
-                                            technique,
-                                            value=checkbox_states[technique],
-                                            on_change=lambda e, t=technique: checkbox_states.__setitem__(t, e.value)
-                                        )
-
-                            ui.button('Apply filters', color='#808080',
-                                      on_click=lambda: create_on_click()).classes(
-                                'w-[270px] h-[30px] rounded-[8px] text-white bg-[rgb(44, 44, 44)] self-end')
+                        # with ui.column().classes(
+                        #         'mt-[20px] max-w-[300px] rounded-[12px] p-[20px] shadow-md'):
+                        #     ui.label('Filters:').classes('text-xl font-bold mb-4')
+                        #     ui.label('Show manipulation techniques:').classes('text-[16px] mt-[10px] mb-[5px]')
+                        #
+                        #     with ui.column().classes(
+                        #             'max-h-[300px] w-[200px] overflow-y-auto rounded-[12px] p-[20px] shadow-md'):
+                        #         for technique in checkbox_states.keys():
+                        #             if technique != 'none':
+                        #                 checkbox_elements[technique] = ui.checkbox(
+                        #                     technique,
+                        #                     value=checkbox_states[technique],
+                        #                     on_change=lambda e, t=technique: checkbox_states.__setitem__(t, e.value)
+                        #                 )
+                        #
+                        #     ui.button('Apply filters', color='#808080',
+                        #               on_click=lambda: create_on_click()).classes(
+                        #         'w-[270px] h-[30px] rounded-[8px] text-white bg-[rgb(44, 44, 44)] self-end')
 
                         with ui.column().classes('mt-[20px] w-[1000px] w-max-[1300px]'):
                             with ui.card().classes('w-full'):
@@ -220,14 +224,31 @@ def render_analyze_manipulations_request():
                                 with ui.row().classes('w-[430px] self-end gap-0'):
                                     ui.button('Download JSON', color='green', on_click=call_download_json).classes(
                                         'w-[210px] rounded-[8px] h-[30px] m-0 mr-[10px] self-end')
-                                    ui.button('Clear', color='#808080', on_click=lambda: (card_container.clear(),
+                                    ui.button('Clear', color='#2c2c2c', on_click=lambda: (card_container.clear(),
                                                                                           clear_action())).classes(
                                         'w-[210px] h-[30px] rounded-[8px] text-white bg-[rgb(44, 44, 44)] self-end')
 
-            with ui.column().classes('items-center gap-0 self-end'):
-                ui.button('Send a request', color='#2c2c2c', on_click=create_on_click).classes(
-                    'w-[270px] h-[40px] rounded-[8px] text-white bg-[rgb(44, 44, 44)] ml-[20px] mt-[10px] mt-[46px] '
-                    'self-end')
+                    ui.run_javascript('window.scrollTo({ top: 0, behavior: "smooth" });')
+
+            with start_column:
+                with ui.row().classes('w-full mt-[10px] justify-between items-center'):
+                    with ui.dropdown_button('Filters',  color='#2c2c2c').classes(
+                            'w-[270px]  rounded-[8px] text-white bg-[rgb(44,44,44)] h-[40px] '
+                    ):
+                        ui.label('Show manipulation techniques:').classes('text-[16px] mt-[10px] mb-[5px]')
+                        with ui.column().classes(
+                                'max-h-[300px] w-[200px] overflow-y-auto rounded-[12px] p-[20px] shadow-md'):
+                            for technique in checkbox_states.keys():
+                                if technique != 'none':
+                                    checkbox_elements[technique] = ui.checkbox(
+                                        technique,
+                                        value=checkbox_states[technique],
+                                        on_change=lambda e, t=technique: checkbox_states.__setitem__(t, e.value)
+                                    )
+
+                    ui.button('Send a request', color='#2c2c2c', on_click=create_on_click).classes(
+                        'w-[270px] h-[40px] rounded-[8px] text-white bg-[rgb(44,44,44)]'
+                    )
 
     render_footer()
 
